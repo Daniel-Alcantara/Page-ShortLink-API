28570a36ee1cc42d1e4bfede153e41540954537a
Daniel Alcantara <112096248+Daniel-Alcantara@users.noreply.github.com>
2022-11-29 14:21:40 -0300

    Finalização API e Desktop

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    $("#gerator_encurtador").click(function () {
+        var link = $("#text_value").val()
+        shortlink(link)
+    });
+
+    $(".short_copy").click(function () {
+        var palavra = $("#teste").text()
+        navigator.clipboard.writeText(palavra)
+
+        $(".short_copy button").text("Copied")
+        $(".short_copy button").css("background", "#3a3053")
+    });
+    
+});
+
+
+function shortlink(valor) {
+    var BaseUrl = "https://api.shrtco.de/v2/"
+    var url = BaseUrl + "shorten?url=" + valor
+    console.log(url)
+
+    $.ajax({
+        url: url,
+        type: "GET",
+    }).done(function (response) {
+        var link_encurtado = response.result.short_link
+
+        $(".short_generator").css("display", "block")
+        $(".short_link_generator").text(valor)
+        $(".short_gerator").text(link_encurtado)
+
+    })
+
+}
